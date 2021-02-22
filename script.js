72d7f9376ed8964eeb048481738ab069632306ff
SophiaSamera <Sophia.samera@ocsbstudent.ca>
2021-02-22 14:16:45 +0000

    fixed javascript and added little bit of css

--- script.js
+++ script.js
@@ -4,11 +4,9 @@ let userInput = 0
 function comparison () {
   userInput = document.getElementById('input').value
   userInput = parseInt(userInput)
-
   if (userInput < 0) {
     document.getElementById('answer').innerHTML = 'Negative'
   } else {
     document.getElementById('answer').innerHTML = 'Positive'
   }
-
 }
